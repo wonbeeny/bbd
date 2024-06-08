b26d94f64bf45f317d004b96ebefb553590c9211
wonbeeny <wonbeeny@gmail.com>
2024-06-08 14:37:39 +0900

    add - for amount

--- src/bbd/utils/amount_utils.py
+++ src/bbd/utils/amount_utils.py
@@ -7,7 +7,7 @@ def format_amount(input_amount: str) -> str:
     """ 
     숫자와 [`.`, `,`]을 제외한 모든 문자를 공백으로 치환
     """
-    amount = re.sub(r"[^0-9\.,]", "", input_amount)
+    amount = re.sub(r"[^0-9\.,-]", "", input_amount)
     
     try:
         float(amount.replace(",", ""))
